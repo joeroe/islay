79042a4ea93d90e56e6aeec9e7059719d75ea872
Joe Roe <joe@joeroe.io>
2023-03-14 10:34:36 +0100

    Update islay_sites documentation

--- R/data.R
+++ R/data.R
@@ -8,23 +8,18 @@
 #' \describe{
 #'   \item{site_name}{Full name of the site, e.g. `"Kindrochid 4"`.}
 #'   \item{site_code}{Abbreviated code for the site, where given, e.g. `"KRD4"`. Can be `NA`.}
-#'   \item{provenance}{Source of information on the site. See details.}
 #'   \item{period}{Period classification based on chipped stone assemblage. See details.}
+#'   \item{longitude,latitude}{Geographic coordinates of the site.}
+#'   \item{total_chipped_stone}{Total number of chipped stone artefacts (lithics) collected at the site.}
+#'   \item{source}{Source of information on the site. See details.}
 #' }
 #'
 #' @details
-#' `provenance` can be one of:
+#' `source` can be one of:
 #' * `"Literature"` – sites documented before the SHMP
 #' * `"Collection"` – private or museum collections
 #' * `"Quarry"` – finds eroded from a quarry face
 #' * `"Fieldwalking"` – fields systematically surveyed by the SHMP
-#'
-#' `period` can be one of:
-#' * `"M"` – definite Mesolithic activity, no later prehistoric activity
-#' * `"ML"` – definite Mesolithic activity, later prehistoric activity
-#' * `"PM"` – probable Mesolithic activity, no later prehistoric activity
-#' * `"L"` – no Mesolithic activity, definite later prehistoric activity
-#' * `"U"` – unknown
 #'
 #' @references
 #' \insertRef{SHMP4.2}{islay}
