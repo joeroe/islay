6364ec5e9ab20ae5c141e7e3e7f56376c987df08
Joe Roe <joe@joeroe.io>
2023-03-14 10:28:52 +0100

    Add variables to islay_sites
    Coordinates, total chipped stone, and expanded period labels

--- data-raw/islay_sites.R
+++ data-raw/islay_sites.R
@@ -1,4 +1,4 @@
 # Prepare dataset `islay_sites`
 
-islay_sites <- readr::read_tsv("data-raw/islay_sites.tsv", col_types = "cccc")
+islay_sites <- readr::read_tsv("data-raw/islay_sites.tsv", col_types = "cccdddc")
 usethis::use_data(islay_sites, overwrite = TRUE)
